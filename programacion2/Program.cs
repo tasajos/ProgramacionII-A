d5320ebca3da6d309462183c7fab62df827e95a0
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-10 21:02:26 -0400

    init3
    init

--- programacion2/Program.cs
+++ programacion2/Program.cs
@@ -16,27 +16,53 @@
             Console.WriteLine("Por favor dinos tu nombre");
             string nombre = Console.ReadLine();
             Console.WriteLine("Bienvenido " +  nombre);
-            Console.WriteLine("Bienvenido a la carrera de IA");
             Console.WriteLine("por favor dinos en que semestre estas");
-            string carrera = Console.ReadLine();
+            string semestre = Console.ReadLine();
             Console.WriteLine("Vamos a interactuar contigo y realizar funciones y metodos");
             Console.WriteLine("por favor ingresa un numero para A");
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("por favor ingresa un numero para B");
             int b = int.Parse(Console.ReadLine());
 
+            /*
             int suma;
-
             suma = a + b;
-            
             Console.WriteLine("el resultado es " + suma);
-
+            */
+            Console.WriteLine("La suma es " + sumar(a, b));
+            Console.WriteLine("La resta es " + resta(a, b));
+            Console.WriteLine("La multiplicacion es " + multiplicacion(a, b));
+            Console.WriteLine("La division es " + division(a, b));
+            /*
+             * Metodos
+             */
             static void saludar()
             {
                 Console.WriteLine("UNICEN bienvenidos al nuevo mundo este es un metodo");
-                Console.Write("PROGRAMACION II - rules");
+                Console.WriteLine("PROGRAMACION II - rules");
+            }
+            /*
+             * Funciones
+             */
+            static  double sumar(double a , double b)
+            {
+                return (a + b);
             }
 
+            static double resta(double a, double b)
+            {
+                return (a - b);
+            }
+
+            static double multiplicacion(double a, double b)
+            {
+                return (a - b);
+            }
+
+            static double division(double a, double b)
+            {
+                return (a / b);
+            }
 
         }
     }
