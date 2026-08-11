f53ae44f2bbbcf01567da93185f27b7b342576da
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-10 20:51:32 -0400

    init1

--- programacion2/Program.cs
+++ programacion2/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            /*
+
+            Console.WriteLine("UNICEN");
+            Console.WriteLine("UNICEN bienvenidos al nuevo mundo");
+            Console.Write("PROGRAMACION II");
+            */
+
+            saludar();
+            Console.WriteLine("Por favor dinos tu nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido " +  nombre);
+            Console.WriteLine("Bienvenido a la carrera de IA");
+            Console.WriteLine("por favor dinos en que semestre estas");
+            string carrera = Console.ReadLine();
+            Console.WriteLine("Vamos a interactuar contigo y realizar funciones y metodos");
+            Console.WriteLine("por favor ingresa un numero para A");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("por favor ingresa un numero para B");
+            int b = int.Parse(Console.ReadLine());
+
+            int suma;
+
+            suma = a + b;
+            
+            Console.WriteLine("el resultado es " + suma);
+
+            static void saludar()
+            {
+                Console.WriteLine("UNICEN bienvenidos al nuevo mundo este es un metodo");
+                Console.Write("PROGRAMACION II - rules");
+            }
+
+
         }
     }
 }
