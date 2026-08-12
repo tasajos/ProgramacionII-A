daf27853cfeda9729404cdb5274fba0d41cdd3f7
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-11 21:54:42 -0400

    init clase 2
    clase 2

---
+++ programacion2/Clase02/Condicionantes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programacion2.Clase02
+{
+    public class Condicionantes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("||===========================||");
+            Console.WriteLine("||===========================||");
+            Console.WriteLine("||==B==I==E==N=V=E=N=I=D=O=S=||");
+            Console.WriteLine("||===========================||");
+            Console.WriteLine("||===========================||");
+            Console.WriteLine("||=====C==L==A==S=E====2=====||");
+            Console.WriteLine("===============================");
+
+
+            funciones_metodos.SALUDAR();
+            //funciones_metodos.datosentrada();
+            var (a, b) = funciones_metodos.datosentrada();
+            funciones_metodos.validacion(a, b);
+
+        }
+
+
+       
+    }
+}
---
+++ programacion2/Clase02/funciones_metodos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programacion2.Clase02
+{
+    public class funciones_metodos
+    {
+
+        public static void SALUDAR()
+        {
+            Console.WriteLine(">>>>>>>CLASE 02<<>>>>>");
+            Console.WriteLine(">>>>CONDICIONANTES<<<<");
+        }
+
+        /*
+        public static double datosentrada(double a , double b)
+        {
+            Console.WriteLine("Ingresa un valor para a");
+            double a = double.Parse(Console.ReadLine());
+            return (a+ b);
+        }
+        */
+
+        public static  (double,double) datosentrada()
+        {
+            Console.WriteLine("Ingresa un valor para a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa un valor para b");
+            double b = double.Parse(Console.ReadLine());
+
+            return (a,b);
+
+        }
+
+        public static double validacion(double a, double b)
+        {
+            //Console.WriteLine("Ingresa un valor para a");
+            //double a = double.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("El numero es mayor");
+                return a;
+            }else
+                if (a < b) {
+                Console.WriteLine("El numero " + b + " Es mayor ");
+                return b;
+                    }
+
+            return 0;
+
+
+    }
+
+    }
+}
